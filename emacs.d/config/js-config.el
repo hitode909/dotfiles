74262699ee384ace8cbfc7ed23bbfe914bd917ee
hitode909 <hitode909@gmail.com>
2011-05-08 10:20:38 +0900

    js config

--- emacs.d/config/js-config.el
+++ emacs.d/config/js-config.el
@@ -1,33 +1,44 @@
 (when nil
-;;js2-mode
-(setq-default c-basic-offset 4)
-(autoload 'js2-mode "js2" nil t)
+  ;;js2-mode
+  (setq-default c-basic-offset 4)
+  (autoload 'js2-mode "js2" nil t)
+  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+  (add-hook 'js2-mode-hook
+            '(lambda ()
+               (setq js2-cleanup-whitespace nil
+                     js2-mirror-mode nil
+                     js2-bounce-indent-flag t)
+
+               (defun indent-and-back-to-indentation ()
+                 (interactive)
+                 (indent-for-tab-command)
+                 (let ((point-of-indentation
+                        (save-excursion
+                          (back-to-indentation)
+                          (point))))
+                   (skip-chars-forward "\s " point-of-indentation)))
+               (define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)
+               (define-key js2-mode-map "\C-m" nil)
+               (define-key js2-mode-map "\C-cm" 'next-error)
+               ))
+
+  )
+
+(when nil
+  ;; espesso-mode
+  ;; http://www.nongnu.org/espresso/
+  (autoload 'espresso-mode "espresso" "Start espresso-mode" t)
+  (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
+  (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
+  )
+
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook
           '(lambda ()
-             (setq js2-cleanup-whitespace nil
-                   js2-mirror-mode nil
-                   js2-bounce-indent-flag t)
-
-             (defun indent-and-back-to-indentation ()
-               (interactive)
-               (indent-for-tab-command)
-               (let ((point-of-indentation
-                      (save-excursion
-                        (back-to-indentation)
-                        (point))))
-                 (skip-chars-forward "\s " point-of-indentation)))
-             (define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)
-             (define-key js2-mode-map "\C-m" nil)
-             (define-key js2-mode-map "\C-cm" 'next-error)
-             ))
-
-)
-;; espesso-mode
-;; http://www.nongnu.org/espresso/
-(autoload 'espresso-mode "espresso" "Start espresso-mode" t)
-(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
+               (define-key js2-mode-map "\C-m" nil)
+               (define-key js2-mode-map "\C-cm" 'next-error)
+               ))
 
 ;;moz-plus
 (autoload 'run-mozilla "moz" "run inferior moz" t)
@@ -38,8 +49,8 @@
 
 
 (when nil
-(require 'actionscript-mode)
-(setq auto-mode-alist
-      (append '(("\\.as$" . actionscript-mode))
-              auto-mode-alist))
-)
+  (require 'actionscript-mode)
+  (setq auto-mode-alist
+        (append '(("\\.as$" . actionscript-mode))
+                auto-mode-alist))
+  )
