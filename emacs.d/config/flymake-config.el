f5f937c4ee7b462ae7b00243838ff2362af6ac44
hitode909 <hitode909@gmail.com>
2011-05-08 21:14:36 +0900

    flymake coffeescript

--- emacs.d/config/flymake-config.el
+++ emacs.d/config/flymake-config.el
@@ -96,3 +96,32 @@
 
 (delete '("\\.html?\\'" flymake-xml-init) flymake-allowed-file-name-masks)
 (delete '("\\.xml\\'" flymake-xml-init) flymake-allowed-file-name-masks)
+
+;; flymake coffeescript
+
+(setq flymake-coffeescript-err-line-patterns
+  '(("\\(Error: In \\([^,]+\\), .+ on line \\([0-9]+\\).*\\)" 2 3 nil 1)))
+
+(defconst flymake-allowed-coffeescript-file-name-masks
+  '(("\\.coffee$" flymake-coffeescript-init)))
+
+(defun flymake-coffeescript-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "coffee" (list local-file))))
+
+(defun flymake-coffeescript-load ()
+  (interactive)
+  (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+    (setq flymake-check-was-interrupted t))
+  (ad-activate 'flymake-post-syntax-check)
+  (setq flymake-allowed-file-name-masks
+        (append flymake-allowed-file-name-masks
+                flymake-allowed-coffeescript-file-name-masks))
+  (setq flymake-err-line-patterns flymake-coffeescript-err-line-patterns)
+  (flymake-mode t))
+
+(add-hook 'coffee-mode-hook 'flymake-coffeescript-load)
