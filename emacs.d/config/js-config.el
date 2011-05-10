c190ba511337be452ab97304c1a3224c9b199c2b
hitode909 <hitode909@gmail.com>
2011-05-11 07:34:11 +0900

    compile file

--- emacs.d/config/js-config.el
+++ emacs.d/config/js-config.el
@@ -61,10 +61,13 @@
   "coffee-mode-hook"
  (set (make-local-variable 'tab-width) 2)
  (setq coffee-cleanup-whitespace nil)
- (define-key coffee-mode-map [(meta R)] 'coffee-compile-buffer)
+ (define-key coffee-mode-map [(meta R)] 'coffee-compile-file)
  )
 
 (add-hook 'coffee-mode-hook
   '(lambda() (coffee-custom)))
 
 (add-to-list 'auto-mode-alist '("\\.js.shd$" . coffee-mode))
+
+
+(setq coffee-args-compile '("-cb"))
