dbc13cae223e3ad8b6488e953ad0e9ccbbee4a0d
hitode909 <hitode909@gmail.com>
2011-05-26 10:48:38 +0900

    anything config

--- emacs.d/config/anything-config.el
+++ emacs.d/config/anything-config.el
@@ -29,18 +29,19 @@
 
 
 (global-set-key (kbd "C-;") 'anything)
-(setq anything-sources (list anything-c-source-buffers
+(setq anything-sources (list 
                              ;anything-c-source-project
                              ;anything-c-source-imenu
                              anything-c-source-bookmarks
                              ;; anything-c-source-file-name-history
-                             anything-c-source-files-in-current-dir
                              ;; anything-c-source-projects-history ;; I need!!!!!
                              ;; anything-c-source-mac-spotlight
                              nym:anything-find-file   ;; cool
                              ;;anything-c-source-complete-ruby-all
                              ;;anything-c-source-occur-by-moccur
                              anything-c-source-recentf
+                             anything-c-source-buffers
+                             anything-c-source-files-in-current-dir
                              ;; anything-c-source-emacs-functions
                              ;; anything-c-source-google-suggest
                              anything-c-source-emacs-commands
