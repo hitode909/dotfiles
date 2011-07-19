e93ccf61f9843ee2c1c4a96fc9648a50f0018ddd
hitode909 <hitode909@gmail.com>
2011-07-20 08:40:54 +0900

    fix

--- emacs.d/config/smartchr-config.el
+++ emacs.d/config/smartchr-config.el
@@ -5,15 +5,19 @@
 ;; (global-set-key (kbd "!") (smartchr '("!" "!!" " != ")))
 ;; (global-set-key (kbd "<") (smartchr '("<" "<<" " < " " <= ")))
 ;; (global-set-key (kbd "{") (smartchr '("{ `!!' }" "{")))
-(define-key cperl-mode-map (kbd ">") (smartchr '(">" " => " ">>" " > " " >= " " => '`!!''" " => \"`!!'\"")))
-(define-key cperl-mode-map (kbd "F") (smartchr '("F" "$" "$_" "$_->" "@$")))
-(define-key cperl-mode-map (kbd "M") (smartchr '("M" "my $`!!' = ")))
-(define-key cperl-mode-map (kbd "D") (smartchr '("D" "use Data::Dumper; warn Dumper `!!';")))
-(define-key cperl-mode-map (kbd "S") (smartchr '("S" "my ($self) = @_;" "my ($self, $`!!') = @_;")))
-(define-key cperl-mode-map (kbd ".") (smartchr '("->" "." "..")))
-(define-key cperl-mode-map (kbd "|") (smartchr '(" || " "|")))
-(define-key cperl-mode-map (kbd "&") (smartchr '(" && " "&")))
-(define-key cperl-mode-map (kbd "{") (smartchr '("{" "sub {")))
+(add-hook 'cperl-mode-hook
+          '(lambda ()
+             (define-key cperl-mode-map (kbd ">") (smartchr '(">" " => " ">>" " > " " >= " " => '`!!''" " => \"`!!'\"")))
+             (define-key cperl-mode-map (kbd "F") (smartchr '("F" "$" "$_" "$_->" "@$")))
+             (define-key cperl-mode-map (kbd "M") (smartchr '("M" "my $`!!' = ")))
+             (define-key cperl-mode-map (kbd "D") (smartchr '("D" "use Data::Dumper; warn Dumper `!!';")))
+             (define-key cperl-mode-map (kbd "S") (smartchr '("S" "my ($self) = @_;" "my ($self, $`!!') = @_;")))
+             (define-key cperl-mode-map (kbd ".") (smartchr '("->" "." "..")))
+             (define-key cperl-mode-map (kbd "|") (smartchr '(" || " "|")))
+             (define-key cperl-mode-map (kbd "&") (smartchr '(" && " "&")))
+             (define-key cperl-mode-map (kbd "{") (smartchr '("{" "sub {")))
+
+             ))
 
 ;; (define-key espresso-mode-map (kbd "L") (smartchr '("L" "console.log(`!!');")))
 (add-hook 'ruby-mode-hook
