ba8e7a045fcc00326579017ce6c18c5f76a319fb
hitode909 <hitode909@gmail.com>
2011-08-12 23:00:47 +0900

    coffee

--- emacs.d/config/js-config.el
+++ emacs.d/config/js-config.el
@@ -56,24 +56,25 @@
   )
 
 
-(require 'coffee-mode)
-(defun coffee-custom ()
-  "coffee-mode-hook"
- (set (make-local-variable 'tab-width) 2)
- (setq coffee-cleanup-whitespace nil)
- (define-key coffee-mode-map [(meta R)] 'coffee-compile-file)
+(when (require 'coffee-mode nil t)
+  (defun coffee-custom ()
+    "coffee-mode-hook"
+    (set (make-local-variable 'tab-width) 2)
+    (setq coffee-cleanup-whitespace nil)
+    (define-key coffee-mode-map [(meta R)] 'coffee-compile-file)
 
- (add-hook 'after-save-hook
-           '(lambda ()
-              (when (string-match "\.coffee" (buffer-name)) ; ~~~.coffee<>
-                (coffee-compile-file))))
- )
+    (add-hook 'after-save-hook
+              '(lambda ()
+                 (when (string-match "\.coffee" (buffer-name)) ; ~~~.coffee<>
+                   (coffee-compile-file))))
+    )
 
-(add-hook 'coffee-mode-hook
-  '(lambda() (coffee-custom)))
+  (add-hook 'coffee-mode-hook
+            '(lambda() (coffee-custom)))
 
-(add-to-list 'auto-mode-alist '("\\.js.shd$" . coffee-mode))
+  (add-to-list 'auto-mode-alist '("\\.js.shd$" . coffee-mode))
 
 
-(setq coffee-args-compile '("-cb"))
-(setq ac-modes (cons 'coffee-mode ac-modes))
+  (setq coffee-args-compile '("-cb"))
+  (setq ac-modes (cons 'coffee-mode ac-modes))
+  )
