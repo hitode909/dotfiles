ffd43ee2e8d8cf8d6edd9bf8ff4075f48a25f38f
hitode909 <hitode909@gmail.com>
2011-09-26 00:49:09 +0900

    timer

--- emacs.d/config/basic-config.el
+++ emacs.d/config/basic-config.el
@@ -544,3 +544,80 @@
 
 ;; window分割しない
 (setq pop-up-windows nil)
+
+
+
+
+;; --------- タイマー系 選択時にだけチェックするように -------------
+;; ここでやるの見苦しいからアドバイスで処理したい
+
+(defun auto-revert-handler ()
+  "Revert current buffer, if appropriate.
+This is an internal function used by Auto-Revert Mode."
+  (when (and (eq (window-buffer (selected-window)) (current-buffer)) (or auto-revert-tail-mode (not (buffer-modified-p))))
+    (let* ((buffer (current-buffer)) size
+	   (revert
+	    (or (and buffer-file-name
+		     (file-readable-p buffer-file-name)
+		     (if auto-revert-tail-mode
+			 ;; Tramp caches the file attributes.  Setting
+			 ;; `tramp-cache-inhibit' forces Tramp to
+			 ;; reread the values.
+			 (let ((tramp-cache-inhibit-cache t))
+			   (/= auto-revert-tail-pos
+			       (setq size
+				     (nth 7 (file-attributes
+					     buffer-file-name)))))
+		       (and (not (file-remote-p buffer-file-name))
+			    (not (verify-visited-file-modtime buffer)))))
+		(and (or auto-revert-mode
+			 global-auto-revert-non-file-buffers)
+		     revert-buffer-function
+		     (boundp 'buffer-stale-function)
+		     (functionp buffer-stale-function)
+		     (funcall buffer-stale-function t))))
+	   eob eoblist)
+      (when revert
+	(when (and auto-revert-verbose
+		   (not (eq revert 'fast)))
+	  (message "Reverting buffer `%s'." (buffer-name)))
+	;; If point (or a window point) is at the end of the buffer,
+	;; we want to keep it at the end after reverting.  This allows
+	;; to tail a file.
+	(when buffer-file-name
+	  (setq eob (eobp))
+	  (walk-windows
+	   #'(lambda (window)
+	       (and (eq (window-buffer window) buffer)
+		    (= (window-point window) (point-max))
+		    (push window eoblist)))
+	   'no-mini t))
+	(if auto-revert-tail-mode
+	    (auto-revert-tail-handler size)
+	  ;; Bind buffer-read-only in case user has done C-x C-q,
+	  ;; so as not to forget that.  This gives undesirable results
+	  ;; when the file's mode changes, but that is less common.
+	  (let ((buffer-read-only buffer-read-only))
+	    (revert-buffer 'ignore-auto 'dont-ask 'preserve-modes)))
+	(when buffer-file-name
+	  (when eob (goto-char (point-max)))
+	  (dolist (window eoblist)
+	    (set-window-point window (point-max)))))
+      ;; `preserve-modes' avoids changing the (minor) modes.  But we
+      ;; do want to reset the mode for VC, so we do it manually.
+      (when (or revert auto-revert-check-vc-info)
+	(vc-find-file-hook)))))
+
+(require 'flymake)
+(defun flymake-on-timer-event (buffer)
+  "Start a syntax check for buffer BUFFER if necessary."
+  (when (and (eq (window-buffer (selected-window)) buffer) (buffer-live-p buffer))
+    (with-current-buffer buffer
+      (when (and (not flymake-is-running)
+		 flymake-last-change-time
+		 (> (- (flymake-float-time) flymake-last-change-time)
+                    flymake-no-changes-timeout))
+
+	(setq flymake-last-change-time nil)
+	(flymake-log 3 "starting syntax check as more than 1 second passed since last change")
+	(flymake-start-syntax-check)))))
