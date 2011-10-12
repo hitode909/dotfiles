ab3c2da2e0601a8df891867ada4fb5fe27612f06
hitode909 <hitode909@gmail.com>
2011-10-12 12:44:17 +0900

    smazencoding setting

--- emacs.d/config/zencoding-mode-config.el
+++ emacs.d/config/zencoding-mode-config.el
@@ -2,11 +2,12 @@
 (require 'zencoding-mode)
 (add-hook 'sgml-mode-hook 'zencoding-mode) ;; Auto-start on any markup modes
 
-(define-key zencoding-mode-keymap (kbd "<C-return>") 'zencoding-expand-line)
-(define-key zencoding-mode-keymap (kbd "C-c C-m") 'zencoding-expand-line)
-(define-key zencoding-preview-keymap (kbd "C-m") 'zencoding-expand-yas)
-(define-key zencoding-preview-keymap (kbd "RET") 'zencoding-expand-yas)
-(define-key zencoding-preview-keymap (kbd "<return>") 'zencoding-expand-yas)
+(define-key zencoding-mode-keymap (kbd "<C-return>") 'zencoding-expand-yas)
+(define-key zencoding-mode-keymap (kbd "C-c C-m") 'zencoding-expand-yas)
+(define-key zencoding-mode-keymap (kbd "C-c C-p") 'zencoding-expand-line)
+;; (define-key zencoding-preview-keymap (kbd "C-m") 'zencoding-expand-yas)
+;; (define-key zencoding-preview-keymap (kbd "RET") 'zencoding-expand-yas)
+;; (define-key zencoding-preview-keymap (kbd "<return>") 'zencoding-expand-yas)
 
 (define-key zencoding-mode-keymap (kbd "C-j") 'skk-mode)
 
