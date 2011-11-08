21aa2854fa08feee53de1601d892a2189b3e5f27
hitode909 <hitode909@gmail.com>
2011-11-08 16:07:55 +0900

    up

--- emacs.d/config/as-config.el
+++ emacs.d/config/as-config.el
@@ -1,2 +1,7 @@
 (when (require 'actionscript-mode nil t)
-  (add-to-list 'auto-mode-alist '("\\.as$" . actionscript-mode)))
+  (add-to-list 'auto-mode-alist '("\\.as$" . actionscript-mode))
+
+  (add-hook 'actionscript-mode-hook
+            '(lambda ()
+               (font-lock-mode 0)))
+  )
