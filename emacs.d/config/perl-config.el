15b9ac5ca900c92bb1a3a8c87a0fecd787ad3f54
hitode909 <hitode909@gmail.com>
2011-12-04 14:52:10 +0900

    perl test

--- emacs.d/config/perl-config.el
+++ emacs.d/config/perl-config.el
@@ -2,10 +2,27 @@
 (setq auto-mode-alist
       (append '(("\\.\\([pP][Llm]\\|t\\)$" . cperl-mode))  auto-mode-alist ))
 
-(defun my-run-test ()
+(defun run-perl-test ()
   "test実行します"
   (interactive)
   (compile (format "cd  %s; %s -MProject::Libs %s" (replace-regexp-in-string "\n+$" "" (shell-command-to-string "git rev-parse --show-cdup")) (expand-file-name "~/perl5/perlbrew/perls/current/bin/perl") (buffer-file-name (current-buffer)))))
+
+
+(defun run-perl-method-test ()
+  (interactive)
+  (let (
+        (command compile-command)
+        (test-method nil))
+    (save-excursion
+      (when (or
+             (re-search-backward "\\bsub\s+\\([_[:alpha:]]+\\)\s*:\s*Test" nil t)
+             (re-search-forward "\\bsub\s+\\([_[:alpha:]]+\\)\s*:\s*Test" nil t))
+        (setq test-method (match-string 1))))
+    (if test-method
+        (compile (format "cd  %s; TEST_METHOD=%s %s -MProject::Libs %s" (replace-regexp-in-string "\n+$" "" (shell-command-to-string "git rev-parse --show-cdup")) test-method (expand-file-name "~/perl5/perlbrew/perls/current/bin/perl") (buffer-file-name (current-buffer))))
+        (compile (format "cd  %s; %s -MProject::Libs %s" (replace-regexp-in-string "\n+$" "" (shell-command-to-string "git rev-parse --show-cdup")) (expand-file-name "~/perl5/perlbrew/perls/current/bin/perl") (buffer-file-name (current-buffer)))))))
+
+
 
 ;; インデントにタブを使わない
 (add-hook 'cperl-mode-hook
@@ -23,7 +40,8 @@
              ;; (add-to-list 'ac-sources 'ac-source-perl-completion)
              ;; (perl-completion-mode t)
 
-             (define-key cperl-mode-map [(super t)] 'my-run-test)
+             (define-key cperl-mode-map [(super T)] 'run-perl-test)
+             (define-key cperl-mode-map [(super t)] 'run-perl-method-test)
              ))
 
 (setq-default indent-tabs-mode nil)
