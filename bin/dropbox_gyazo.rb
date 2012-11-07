96bc083f14600fb1c0d112b8e6dc0063b854b9bc
hitode909 <hitode909@gmail.com>
2012-11-07 19:44:56 +0900

    画像できるの待つ

--- bin/dropbox_gyazo.rb
+++ bin/dropbox_gyazo.rb
@@ -2,6 +2,7 @@
 
 require 'fileutils'
 require 'digest/md5'
+require 'open-uri'
 
 file = File.expand_path "~/Dropbox/Public/g/#{Digest::MD5.hexdigest(Time.now.to_f.to_s)}.png"
 url = 'http://dl.dropbox.com/u/8270034/g/' + File.basename(file)
@@ -11,6 +12,13 @@ FileUtils.mkdir_p File.dirname(file)
 system "screencapture -i \"#{file}\""
 if File.exist?(file) then
   system "sips -d profile --deleteColorManagementProperties \"#{file}\""
+end
+
+begin
+  puts open(url).read
+rescue
+  sleep 0.3
+  retry
 end
 
 system "echo -n #{url} | pbcopy"
