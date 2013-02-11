ed96bbd42ffcc20cea6e92bb1beebeebf4fcb556
hitode909 <hitode909@gmail.com>
2013-02-12 02:06:04 +0900

    face

--- emacs.d/config/basic-config.el
+++ emacs.d/config/basic-config.el
@@ -437,7 +437,8 @@
 ;; jaunte
 (require 'jaunte)
 (global-set-key [(super j)] 'jaunte)
-
+(set-face-foreground 'jaunte-hint-face "#fdf6e3")
+(set-face-background 'jaunte-hint-face "#d33682")
 
 ;; pos-tip
 (require 'pos-tip)
