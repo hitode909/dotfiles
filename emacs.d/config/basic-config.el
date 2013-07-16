d12d4383f049301521837dbcfaf7fcb089e386f6
hitode909 <hitode909@gmail.com>
2013-07-16 10:06:15 +0900

    config

--- emacs.d/config/basic-config.el
+++ emacs.d/config/basic-config.el
@@ -771,3 +771,8 @@ This is an internal function used by Auto-Revert Mode."
 (require 'google-translate)
 (global-set-key "\C-ct" 'google-translate-at-point)
 (global-set-key "\C-cT" 'google-translate-query-translate)
+
+
+(custom-set-variables
+  '(google-translate-default-source-language "en")
+  '(google-translate-default-target-language "ja"))
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -34,3 +34,18 @@
 
 
 (message "hi")
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(google-translate-default-source-language "en")
+ '(google-translate-default-target-language "ja")
+ '(magit-default-tracking-name-function (quote magit-default-tracking-name-branch-only)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(flymake-errline ((((class color)) (:underline "red"))))
+ '(flymake-warnline ((((class color)) (:underline "yellow")))))
