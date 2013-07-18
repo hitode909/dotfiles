ae0a9d2516995b415a7b1b32fe428841296fdd67
hitode909 <hitode909@gmail.com>
2013-07-18 17:25:39 +0900

    key remap

---
+++ kr4mb-import.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+
+$cli set remap.simultaneouskeypresses_mouse_keys_mode_df 1
+/bin/echo -n .
+$cli set option.emacsmode_optionD 1
+/bin/echo -n .
+$cli set option.mousekeys_mode_uio2click 1
+/bin/echo -n .
+$cli set option.emacsmode_controlM 1
+/bin/echo -n .
+$cli set option.emacsmode_controlH 1
+/bin/echo -n .
+$cli set option.emacsmode_controlW 1
+/bin/echo -n .
+$cli set repeat.wait 0
+/bin/echo -n .
+$cli set option.emacsmode_controlV 1
+/bin/echo -n .
+$cli set option.emacsmode_ex_controlG 1
+/bin/echo -n .
+$cli set option.emacsmode_optionV 1
+/bin/echo -n .
+$cli set option.emacsmode_controlK 1
+/bin/echo -n .
+$cli set option.emacsmode_controlPNBF 1
+/bin/echo -n .
+$cli set option.emacsmode_controlY 1
+/bin/echo -n .
+$cli set option.emacsmode_OptionWCopy 1
+/bin/echo -n .
+$cli set remap.space2shiftL_space 1
+/bin/echo -n .
+$cli set option.emacsmode_controlD 1
+/bin/echo -n .
+$cli set option.emacsmode_controlI 1
+/bin/echo -n .
+$cli set repeat.initial_wait 0
+/bin/echo -n .
+/bin/echo
